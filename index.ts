d33a6ac0c12e4afcff747b96e70ac417ddd8c177
Dmitry Litsman <d@litsman.com>
2024-04-05 00:10:27 +0100

    wip some ideas

--- index.ts
+++ index.ts
@@ -1,63 +1,89 @@
-import { input, confirm } from "@inquirer/prompts";
+// import { input, confirm } from "@inquirer/prompts";
 
-const answers = {
-  firstName: await input({ message: "What's your first name?" }),
-  allowEmail: await confirm({ message: "Do you allow us to send you email?" }),
-};
+// const answers = {
+//   firstName: await input({ message: "What's your first name?" }),
+//   allowEmail: await confirm({ message: "Do you allow us to send you email?" }),
+// };
 
-console.log("!!!", answers);
+// console.log("!!!", answers);
 
 import * as commander from "commander";
 const program = new commander.Command();
 
-// This example shows a simple use of configureHelp.
-// This is used as an example in the README.
-// Any method on the Help class can be overridden
-// See: https://github.com/tj/commander.js/blob/master/lib/help.js
+const brew = program.command("brew");
+brew.command("tea");
+brew.command("coffee");
 
-program.configureHelp({
-  sortSubcommands: true,
-  subcommandTerm: (cmd) => cmd.name(), // Just show the name, instead of short usage.
-});
+// Add nested commands using `.addCommand().
+// The command could be created separately in another module.
+function makeHeatCommand() {
+  const heat = new commander.Command("heat");
+  heat.command("jug").action(() => {
+    console.log("heat jug");
+  });
+  heat.command("pot").action(() => {
+    console.log("heat pot");
+  });
+  return heat;
+}
+program.addCommand(makeHeatCommand());
 
-program
-  .option("-n, --number <numbers...>", "specify numbers")
-  .option("-l, --letter [letters...]", "specify letters");
+program.parse(process.argv);
 
-program.parse();
+const options = program.opts();
+console.log(program.commands);
+console.log("!!!options", options);
+const limit = options.first ? 1 : undefined;
+console.log(program.args[0].split(options.separator, limit));
 
-console.log("Options: ", program.opts());
-console.log("Remaining arguments: ", program.args);
+// // This example shows a simple use of configureHelp.
+// // This is used as an example in the README.
+// // Any method on the Help class can be overridden
+// // See: https://github.com/tj/commander.js/blob/master/lib/help.js
 
-import * as p from "@clack/prompts";
+// program.configureHelp({
+//   sortSubcommands: true,
+//   subcommandTerm: (cmd) => cmd.name(), // Just show the name, instead of short usage.
+// });
 
-const group = await p.group(
-  {
-    name: () =>
-      p.text({
-        message: "What is your name?",
-        defaultValue: "12312",
-        placeholder: "asdasd",
-      }),
-    age: () => p.text({ message: "What is your age?" }),
-    color: ({ results }) =>
-      p.multiselect({
-        message: `What is your favorite color ${results.name}?`,
-        options: [
-          { value: "red", label: "Red" },
-          { value: "green", label: "Green" },
-          { value: "blue", label: "Blue" },
-        ],
-      }),
-  },
-  {
-    // On Cancel callback that wraps the group
-    // So if the user cancels one of the prompts in the group this function will be called
-    onCancel: ({ results }) => {
-      p.cancel("Operation cancelled.");
-      // process.exit(0);
-    },
-  }
-);
+// program
+//   .option("-n, --number <numbers...>", "specify numbers")
+//   .option("-l, --letter [letters...]", "specify letters");
 
-console.log(group.name, group.age, group.color);
+// program.parse();
+
+// console.log("Options: ", program.opts());
+// console.log("Remaining arguments: ", program.args);
+
+// import * as p from "@clack/prompts";
+
+// const group = await p.group(
+//   {
+//     name: () =>
+//       p.text({
+//         message: "What is your name?",
+//         defaultValue: "12312",
+//         placeholder: "asdasd",
+//       }),
+//     age: () => p.text({ message: "What is your age?" }),
+//     color: ({ results }) =>
+//       p.multiselect({
+//         message: `What is your favorite color ${results.name}?`,
+//         options: [
+//           { value: "red", label: "Red" },
+//           { value: "green", label: "Green" },
+//           { value: "blue", label: "Blue" },
+//         ],
+//       }),
+//   },
+//   {
+//     // On Cancel callback that wraps the group
+//     // So if the user cancels one of the prompts in the group this function will be called
+//     onCancel: ({ results }) => {
+//       p.cancel("Operation cancelled.");
+//       // process.exit(0);
+//     },
+//   }
+// );
+
+// console.log(group.name, group.age, group.color);
